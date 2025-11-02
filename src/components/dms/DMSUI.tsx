30068bfd70e8b58814c79454273654955cf7eae7
WinterSunset95 <wintersunset95@gmail.com>
2025-11-03 03:41:46 +0530

    chore: Remove unnecessary react-pdf CSS imports

--- src/components/dms/DMSUI.tsx
+++ src/components/dms/DMSUI.tsx
@@ -64,8 +64,8 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Document as PdfDocument, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import "react-pdf/dist/esm/Page/TextLayer.css";
+// import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+// import "react-pdf/dist/esm/Page/TextLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
